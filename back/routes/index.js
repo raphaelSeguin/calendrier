88fc3e27f84db55f999454c9efd464f8a405495d
raphaelSeguin <raphaelseguin@gmail.com>
2020-06-12 15:12:14 +0200

    dev env

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,32 +1,34 @@
 const express = require('express');
 const axios   = require('axios');
-const { acuity_user, acuity_key } = require('../config.js');
+try {
+  const { acuity_user, acuity_key } = require('../config.js') || {};
+} catch (e) {
+  console.log('no config file ?')
+}
+
 const router  = express.Router();
-const fs = require('fs');
 const responseMock = require('../response.json');
 
 /* GET home page. */
 router.get('/api/availability/classes/', async function(req, res, next) {
-  // console.log(req.query);
 
-  /*
-  try {
-    const response = await axios.get('https://acuityscheduling.com/api/v1/availability/classes', {
-      auth: { 
-        username: acuity_user,
-        password: acuity_key 
-      },
-      ...req.query
-    })
-    // keep response in a json file
-    // fs.writeFileSync('./response.json', JSON.stringify(response.data));
+  if (process.env.NODE_ENV === 'prodction') {
+    try {
+      const response = await axios.get('https://acuityscheduling.com/api/v1/availability/classes', {
+        auth: { 
+          username: acuity_user,
+          password: acuity_key 
+        },
+        ...req.query
+      })
 
-    res.send(response.data)
-  } catch (e) {
-    console.log(e)
+      res.send(response.data)
+    } catch (e) {
+      console.log(e)
+    }
+  } else {
+    res.send(responseMock)
   }
-  */
-  res.send(responseMock)
 });
 
 module.exports = router;
